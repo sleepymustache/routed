963b9e23c4b854faeb76a4b3b15b455d3ddfa64e
Jaime Rodriguez <jrodriguez@giantagency.com>
2015-10-16 12:32:56 -0700

    Enabled compression by default and moved the cache path back into the sass folder.

--- src/scss/config.rb
+++ src/scss/config.rb
@@ -2,12 +2,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-cache_path = "c:/windows/temp/"
 css_dir = "../css"
-sass_dir = "./"
 images_dir = "../images"
 javascripts_dir = "../js"
-#output_style = :compressed
+output_style = :compressed
 relative_assets = true
+sass_dir = "./"
 sass_options = {:debug_info => true}
 sourcemap = true
