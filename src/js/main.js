7a479ffc12812d614838709d64d997eb6a9e3425
Jaime Rodriguez <hi.i.am.jaime@gmail.com>
2020-12-29 00:58:49 -0800

    update: simple sitemap start

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import Sleepy from './classes/sleepy.class.js';
+import MainNav from './classes/main-nav.class';
 
 const SM = new Sleepy();
 
 SM.ready(() => {
+  new MainNav('nav.main');
   document.getElementsByTagName('body')[0].className = (SM.isTouchDevice()) ? 'touchable' : '';
 });
