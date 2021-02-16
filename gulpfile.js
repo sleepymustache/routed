3d53de383da9f23541fc9b013df0f2f7cb298629
Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
2021-02-16 13:37:26 -0800

    update: enable tests by default

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { dest, parallel, series, src, watch } = require('gulp');
 
 // Configuration
 const devUrl = 'http://localhost:8080'; // The local development URL for BrowserSync
-const enableTests = false;              // Set to true to enable tests
+const enableTests = true;               // Set to false for production
 
 // Gulp plugins
 const browserSync = require('browser-sync').create();
