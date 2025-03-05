80360f6a273c2e8063960d1080aed9602418ac3e
Jaime Rodriguez <hi.i.am.jaime@gmail.com>
2025-03-05 15:24:01 -0800

    update: docker update

--- docker/scripts/watch-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cd /var/www/web/themes/custom/envivent
-npm install --no-audit
-# Compile Node Sass if we need to, first run takes a while
-npm rebuild node-sass
-./node_modules/gulp/bin/gulp.js develop
